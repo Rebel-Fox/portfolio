e4423b16de724c207109a8ed8a20bbd2054f9760
Rebel-Fox <rookiewebdev@outlook.com>
2024-07-18 02:25:35 +0530

    upadated css

--- src/Data/ProjectsData.jsx
+++ src/Data/ProjectsData.jsx
@@ -1,86 +1,77 @@
-const ProjectsData =  [
+const ProjectsData = [
   {
     name: "Quizzical",
     link: "https://quizzical-32gg.onrender.com",
     content:
-      "Trivia_quiz_app_using_React:_fetches_questions_from_OpenTDB_API,_lets_users_answer,_and_shows_their_score.",
+      'font-serif font-medium bg-neutral-200 rounded-lg p-4 hover:relative  hover:after:text-2xl hover:after: hover:after:absolute hover:after:left-0 hover:after:top-0 hover:after:w-full  hover:after:animate-slide-right hover:after:z-10 hover:after:rounded-lg hover:after:backdrop-blur-sm hover:after:flex hover:after:items-center hover:after:px-4 hover:after:content-["Trivia_quiz_app_using_React:_fetches_questions_from_OpenTDB_API,_lets_users_answer,_and_shows_their_score."]',
     img: "../../src/assets/project-photo/quizzical.png",
   },
   {
     name: "Tenzies",
     link: "https://tenzies-9n0j.onrender.com",
     content:
-      "This_is_a_Tenzies_game_built_with_React,_where_players_roll_dice_to_match_values,_tracking_rolls,_time,_and_saving_high_scores.",
+      'font-serif font-medium bg-neutral-200 rounded-lg p-4 hover:relative  hover:after:text-2xl hover:after: hover:after:absolute hover:after:left-0 hover:after:top-0 hover:after:w-full  hover:after:animate-slide-right hover:after:z-10 hover:after:rounded-lg hover:after:backdrop-blur-sm hover:after:flex hover:after:items-center hover:after:px-4 hover:after:content-["This_is_a_Tenzies_game_built_with_React,_where_players_roll_dice_to_match_values,_tracking_rolls,_time,and_saving_high_scores."]',
     img: "../../src/assets/project-photo/Tenzies.png",
   },
   {
     name: "Meme Generator",
     link: "https://meme-generator-ur4w.onrender.com",
     content:
-      "This_is_a_meme_generator_built_with_React,_where_users_can_create_memes_by_adding_text_to_randomly_fetched_images_from_an_API.",
+      'font-serif font-medium bg-neutral-200 rounded-lg p-4 hover:relative  hover:after:text-2xl hover:after: hover:after:absolute hover:after:left-0 hover:after:top-0 hover:after:w-full  hover:after:animate-slide-right hover:after:z-10 hover:after:rounded-lg hover:after:backdrop-blur-sm hover:after:flex hover:after:items-center hover:after:px-4 hover:after:content-["This_is_a_meme_generator_built_with_React_where_users_can_create_memes_by_adding_text_to_randomly_fetched_images_from_an_API."]',
     img: "../../src/assets/project-photo/meme-generator.png",
   },
   {
     name: "Travel Journal",
     link: "https://travel-journal-wa2j.onrender.com/",
-    content:
-      "This_is_a_basic_travel_journal_built_with_React,_featuring_cards_displaying_travel_locations_with_images,_descriptions,_and_links_to_Google_Maps.",
+    content:'font-serif font-medium bg-neutral-200 rounded-lg p-4 hover:relative  hover:after:text-2xl hover:after: hover:after:absolute hover:after:left-0 hover:after:top-0 hover:after:w-full  hover:after:animate-slide-right hover:after:z-10 hover:after:rounded-lg hover:after:backdrop-blur-sm hover:after:flex hover:after:items-center hover:after:px-4 hover:after:content-["This_is_a_basic_travel_journal_built_with_React_featuring_cards_displaying_travel_locations_with_images,_descriptions,_and_links_to_Google_Maps."]',
     img: "../../src/assets/project-photo/travel-journal.png",
   },
   {
     name: "Business Card",
     link: "https://business-card-xem6.onrender.com/",
-    content:
-      "This_is_a_business_card_built_with_React,_featuring_a_profile_picture,_contact_buttons,_an_about_section,_interests,_and_social_media_icons.",
+    content:'font-serif font-medium bg-neutral-200 rounded-lg p-4 hover:relative  hover:after:text-2xl hover:after: hover:after:absolute hover:after:left-0 hover:after:top-0 hover:after:w-full  hover:after:animate-slide-right hover:after:z-10 hover:after:rounded-lg hover:after:backdrop-blur-sm hover:after:flex hover:after:items-center hover:after:px-4 hover:after:content-["This_is_a_business_card_built_with_React,_featuring_a_profile_picture,_contact_buttons_an_about_section,_interests,_and_social_media_icons."]',
     img: "../../src/assets/project-photo/business-card.png",
   },
   {
     name: "Color-picker extension",
     link: "https://color-picker-yjg3.onrender.com",
-    content:
-      "This_is_a_Chrome_extension_built_with_JavaScript,_allowing_users_to_generate_and_copy_color_schemes,_toggle_dark_mode,_and_handle_errors_with_alerts.",
+    content:'font-serif font-medium bg-neutral-200 rounded-lg p-4 hover:relative  hover:after:text-2xl hover:after: hover:after:absolute hover:after:left-0 hover:after:top-0 hover:after:w-full  hover:after:animate-slide-right hover:after:z-10 hover:after:rounded-lg hover:after:backdrop-blur-sm hover:after:flex hover:after:items-center hover:after:px-4 hover:after:content-["This_is_a_Chrome_extension_built_with_JavaScript,_allowing_users_to_generate_and_copy_color_schemes,_toggle_dark_mode,_and_handle_errors_with_alerts."]',
     img: "../../src/assets/project-photo/color-picker.png",
   },
   {
     name: "Movie Watchlist",
     link: "https://movie-watchlist-p95o.onrender.com/index.html",
-    content:
-      "This_is_a_movie_watchlist_application_built_with_JavaScript,_allowing_users_to_search_movies_via_the_OMDb_API,_view_movie_details,_add_movies_to_a_watchlist,_and_store_the_watchlist_in_local_storage.",
+    content:'font-serif font-medium bg-neutral-200 rounded-lg p-4 hover:relative  hover:after:text-2xl hover:after: hover:after:absolute hover:after:left-0 hover:after:top-0 hover:after:w-full  hover:after:animate-slide-right hover:after:z-10 hover:after:rounded-lg hover:after:backdrop-blur-sm hover:after:flex hover:after:items-center hover:after:px-4 hover:after:content-["This_is_a_movie_watchlist_application_built_with_JavaScript_allowing_users_to_search_movies_via_the_OMDb_API,_view_movie_details,_add_movies_to_a_watchlist,_and_store_the_watchlist_in_local_storage."]',
     img: "../../src/assets/project-photo/movie-watchlist.png",
   },
   {
     name: "War Game",
     link: "https://war-rtf0.onrender.com/",
-    content:
-      "This_JavaScript_card_game_lets_users_draw_cards,_compare_values_with_the_computer,_track_scores,_and_decide_the_winner_based_on_remaining_cards.",
+    content:'font-serif font-medium bg-neutral-200 rounded-lg p-4 hover:relative  hover:after:text-2xl hover:after: hover:after:absolute hover:after:left-0 hover:after:top-0 hover:after:w-full  hover:after:animate-slide-right hover:after:z-10 hover:after:rounded-lg hover:after:backdrop-blur-sm hover:after:flex hover:after:items-center hover:after:px-4 hover:after:content-["This_JavaScript_card_game_lets_users_draw_cards,_compare_values_with_the_computer,_track_scores,_and_decide_the_winner_based_on_remaining_cards."]',
     img: "../../src/assets/project-photo/war-game.png",
   },
   {
     name: "Bored Bot",
     link: "https://boredbot.onrender.com",
-    content:
-      "This_code_fetches_an_activity_suggestion_from_a_Bored_API_when_a_button_is_clicked,_updating_the_activity_display_and_adding_a_CSS_class_for_visual_effect.",
+    content:'font-serif font-medium bg-neutral-200 rounded-lg p-4 hover:relative  hover:after:text-2xl hover:after: hover:after:absolute hover:after:left-0 hover:after:top-0 hover:after:w-full  hover:after:animate-slide-right hover:after:z-10 hover:after:rounded-lg hover:after:backdrop-blur-sm hover:after:flex hover:after:items-center hover:after:px-4 hover:after:content-["This_code_fetches_an_activity_suggestion_from_a_Bored_API_when_a_button_is_clicked,_updating_the_activity_display_and_adding_a_CSS_class_for_visual_effect."]',
     img: "../../src/assets/project-photo/bored-bot.png",
   },
   {
     name: "Learning-Journal",
     link: "https://learning-journal-7vxk.onrender.com",
-    content:
-      "Multi_page_learning_journal_with_featured_posts_and_multiple_blogs,_showcasing_learning_journey_and_growth.",
+    content:'font-serif font-medium bg-neutral-200 rounded-lg p-4 hover:relative  hover:after:text-2xl hover:after: hover:after:absolute hover:after:left-0 hover:after:top-0 hover:after:w-full  hover:after:animate-slide-right hover:after:z-10 hover:after:rounded-lg hover:after:backdrop-blur-sm hover:after:flex hover:after:items-center hover:after:px-4 hover:after:content-["Multi_page_learning_journal_with_featured_posts_and_multiple_blogs,_showcasing_learning_journey_and_growth."]',
     img: "../../src/assets/project-photo/learning-journal.png",
   },
   {
     name: "Restaurant Menu",
     link: "https://restaurant-menu-q3if.onrender.com/",
-    content:
-      "A_dynamic_menu_and_order_summary_application_with_interactive_item_management_and_a_modal_for_order_completion.",
+    content:'font-serif font-medium bg-neutral-200 rounded-lg p-4 hover:relative  hover:after:text-2xl hover:after: hover:after:absolute hover:after:left-0 hover:after:top-0 hover:after:w-full  hover:after:animate-slide-right hover:after:z-10 hover:after:rounded-lg hover:after:backdrop-blur-sm hover:after:flex hover:after:items-center hover:after:px-4 hover:after:content-["A_dynamic_menu_and_order_summary_application_with_interactive_item_management_and_a_modal_for_order_completion."]',
     img: "../../src/assets/project-photo/restaurant-menu.png",
   },
   {
     name: "Twimba",
     link: "https://twimba.onrender.com/",
-    content:
-      "Handles_tweet_like,_retweet,_reply,_and_delete_actions,_saves_data_in_local_storage,_and_displays_tweets_and_replies_in_feed,_emulating_Twitter's_functionality.",
+    content:`font-serif font-medium bg-neutral-200 rounded-lg p-4 hover:relative  hover:after:text-2xl hover:after: hover:after:absolute hover:after:left-0 hover:after:top-0 hover:after:w-full  hover:after:animate-slide-right hover:after:z-10 hover:after:rounded-lg hover:after:backdrop-blur-sm hover:after:flex hover:after:items-center hover:after:px-4 hover:after:content-["Handles_tweet_like,_retweet,_reply,_and_delete_actions,_saves_data_in_local_storage,_and_displays_tweets_and_replies_in_feed,_emulating_Twitter's_functionality."]`,
     img: "../../src/assets/project-photo/twimba.png",
   },
 ];
