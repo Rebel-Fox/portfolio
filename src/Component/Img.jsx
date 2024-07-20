cab329da87ffcce35f6d37f4aebe6d8ebbf8230f
Rebel-Fox <rookiewebdev@outlook.com>
2024-07-21 00:06:29 +0530

    added transtion to project images

--- src/Component/Img.jsx
+++ src/Component/Img.jsx
@@ -1,19 +1,25 @@
 import React from "react";
 
-export default function Image({ placeholderSrc, src ,alt,className}) {
+export default function Img({ placeholderSrc, src ,alt,className}) {
 
     const [imgSrc , setImgSrc] = React.useState(placeholderSrc)
-
+    const [imgClass,setImgClass] = React.useState(`${className} blur-md`)
     React.useEffect(()=>{
-        const img = new Image()
-    })
+        const image = new Image()
+        image.src = src
+        image.onload = () =>{
+            setImgClass(`${className} blur-0 transition-[blur] ease-linear duration-500`)
+            setImgSrc(src)
+        }
+
+    },[src])
 
 
   return (
     <img
       src = {imgSrc}
-      alt={alt || ""}
-      className={className}
+      alt={alt}
+      className={imgClass}
     />
   );
 }
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,9 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import ProjectsData from "../Data/ProjectsData";
-// import { LazyLoadImage } from "react-lazy-load-image-component";
-import { Blurhash } from "react-blurhash";
-import Image from "../Component/Img";
+
+import Img from "../Component/Img";
 
 const options = {
   root: null, //means viewport
@@ -14,7 +13,7 @@ const options = {
 export default function Projects() {
   return (
     <div className="flex-1 p-6 text-center ">
-      <h1 className="inline-block mx-auto font-serif font-extrabold text-4xl  uppercase mb-8 relative  after:content-[''] after:absolute after:w-full after:bg-red-500 after:left-0 after:bottom-1  after:h-2 after:-z-10">
+      <h1 className="inline-block mx-auto font-serif font-extrabold text-4xl  uppercase mb-8 relative  after:content-[''] after:absolute after:w-full after:bg-red-500 after:left-0 after:bottom-1  after:h-2 after:-z-10 ">
         projects
       </h1>
       <div className="grid gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
@@ -27,7 +26,7 @@ export default function Projects() {
               rel="noopener noreferrer"
               className={project.content}
             >
-              <Image src={project.img} placeholderSrc={project.placeholder} alt={`${project.name}'s screenshot`} className='w-full object-cover' />
+              <Img src={project.img} placeholderSrc={project.placeholder} alt={`${project.name}'s screenshot`} className='w-full object-cover' />
 
               <p className="mt-1">{project.name}</p>
             </Link>
