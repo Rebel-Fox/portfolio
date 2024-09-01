7f1eac8aaa1a7433f13e3d21a9504bb10f1923cb
Rebel-Fox <rookiewebdev@outlook.com>
2024-09-01 13:35:27 +0530

    added placeholder in contacts page

---
+++ src/Data/ContactsData.jsx
@@ -0,0 +1,28 @@
+const ContactsData = [
+    {
+        name:'Instagram',
+        linkto : 'https://www.instagram.com/vk01068?igsh=dmVqbm9sbjN5bmlk',
+        src : './assets/socials/Instagram-color.svg',
+        isNewTab:true,
+    },
+    {
+        name:'LinkedIn',
+        linkto : 'https://www.linkedin.com/in/vineet-kumar-110772275',
+        src : './assets/socials/Linkedin-color.svg',
+        isNewTab:true,
+    },
+    {
+        name:'Github',
+        linkto : 'https://github.com/Rebel-Fox',
+        src : './assets/socials/Github-color.svg',
+        isNewTab:true,
+    },
+    {
+        name:'Outlook',
+        linkto : 'mailto:rookiewebdev@outlook.com',
+        src : './assets/socials/Outlook-color.svg',
+        isNewTab:false,
+    },
+]
+
+export default ContactsData
--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Form, Link } from "react-router-dom";
 import Img from "../Component/Img";
+import ContactsData from "../Data/ContactsData";
 
 export default function Contacts() {
   return (
@@ -16,67 +17,37 @@ export default function Contacts() {
         </p>
         <p className="text-lg underline">Connect with me on </p>
         <ul className="mt-4 text-xl inline-block space-y-3">
-          <li>
-            <Link
-              to="https://www.instagram.com/vk01068?igsh=dmVqbm9sbjN5bmlk"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="flex items-center gap-3.5 hover:scale-110"
-            >
-              <img
-                src="./assets/socials/Instagram-color.svg"
-                alt="instagram icon"
-                className="w-16 "
-              />
-              <p>Instagram</p>
-            </Link>
-          </li>
-          <li>
-            <Link
-              to="https://www.linkedin.com/in/vineet-kumar-110772275"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="flex items-center gap-3.5 hover:scale-110"
-            >
-              <img
-                src="./assets/socials/Linkedin-color.svg"
-                alt="Linkedin icon"
-                className="w-16"
-              />
-              <p>LinkedIn</p>
-            </Link>
-          </li>
-          <li>
-            <Link
-              to="https://github.com/Rebel-Fox"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="flex items-center gap-3.5 hover:scale-110"
-            >
-              <img
-                src="./assets/socials/Github-color.svg"
-                alt="Github icon"
-                className="w-16"
-              />
-              <p>Github</p>
-            </Link>
-          </li>
-          <li>
-            <Link
-              to="mailto:rookiewebdev@outlook.com"
-              className="flex items-center gap-3.5 hover:scale-110"
-            >
-              <img
-                src="./assets/socials/Outlook-color.svg"
-                alt="Outlook icon"
-                className="w-16"
-              />
-              <p>Consider Mailing me !!</p>
-            </Link>
-          </li>
+          {ContactsData.map(contact => {
+            const [img, setImg] = React.useState(false)
+            React.useEffect(() => {
+              const newImg = new Image();
+              newImg.src = contact.src
+              newImg.onload = () => setImg(true)
+            }, [])
+            return <>
+              <li>
+                <Link
+                  to={contact.linkto}
+                  target="_blank"
+                  {...contact.isNewTab ? { target: "_blank", rel: "noopener noreferrer" }
+                    : {}}
+                  className="flex items-center gap-3.5 hover:scale-110"
+                >
+                  {!img && <div className="w-16 h-16 bg-slate-300"></div>}
+                  {img && <img
+                    src={contact.src}
+                    alt={`${contact.name} icon`}
+                    className="w-16"
+                  />}
+                  <p>{contact.name}</p>
+                </Link>
+              </li>
+            </>
+          })}
         </ul>
       </div>
-      <picture className="p-10 flex items-center justify-center ">
+
+      <picture className="flex-1 p-10 flex items-center justify-center ">
         <Img placeholderSrc='./assets/about-img-small.jpeg' src="./assets/about-img.jpeg" alt="image here " className="w-10/12 sm:w-9/12 xl:w-7/12 rounded-full" />
       </picture>
     </div>
